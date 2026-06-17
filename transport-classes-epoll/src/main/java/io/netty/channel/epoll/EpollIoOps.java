c711c346797f9fdd83414a8fa0089d82c4312d62
Tal Samucha <tal.s@sealsecurity.io>
2026-06-17 10:15:16 +0300

    revert 42577

--- transport-classes-epoll/src/main/java/io/netty/channel/epoll/EpollIoOps.java
+++ transport-classes-epoll/src/main/java/io/netty/channel/epoll/EpollIoOps.java
@@ -51,12 +51,6 @@ public final class EpollIoOps implements IoOps {
 
     public static final EpollIoOps EPOLLET = new EpollIoOps(Native.EPOLLET);
 
-    /**
-     * Special {@link EpollIoOps} which basically means we are not interested in any event and so should remove the
-     * fd from underlying epoll fd.
-     */
-    public static final EpollIoOps NONE = new EpollIoOps(0);
-
     static final int EPOLL_ERR_OUT_MASK = EpollIoOps.EPOLLERR.value | EpollIoOps.EPOLLOUT.value;
     static final int EPOLL_ERR_IN_MASK = EpollIoOps.EPOLLERR.value | EpollIoOps.EPOLLIN.value;
     static final int EPOLL_RDHUP_MASK = EpollIoOps.EPOLLRDHUP.value;
@@ -66,8 +60,7 @@ public final class EpollIoOps implements IoOps {
 
     static {
         EpollIoOps all = new EpollIoOps(EPOLLOUT.value | EPOLLIN.value | EPOLLERR.value | EPOLLRDHUP.value);
-        EVENTS = new EpollIoEvent[all.value + 2];
-        addToArray(EVENTS, NONE);
+        EVENTS = new EpollIoEvent[all.value + 1];
         addToArray(EVENTS, EPOLLOUT);
         addToArray(EVENTS, EPOLLIN);
         addToArray(EVENTS, EPOLLERR);
